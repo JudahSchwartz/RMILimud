b982aa74531b9834c28cef0ec9560f9b560d7834
Judah Schwartz <judahsch@gmail.com>
2018-09-26 12:41:54 -0400

    added shared code

--- src/ClientCode/Client.java
+++ src/ClientCode/Client.java
@@ -1,11 +1,26 @@
 package ClientCode;
 
+import SharedCode.Operations;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
 
 public class Client {
     public static void main(String[] args) {
         System.setSecurityManager(new SecurityManager());
 
         System.out.println("Hello");
+        try {
+            Operations operations = (Operations) Naming.lookup("//localhost/Operations");
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/SharedCode/Operations.java
@@ -0,0 +1,15 @@
+package SharedCode;
+
+import java.io.Serializable;
+import java.rmi.Remote;
+
+public class Operations implements Remote{//, Serializable {
+    static void reverseString(String s)
+    {
+
+    }
+    static void squareNumber(int n)
+    {
+
+    }
+}
